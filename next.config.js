6ee7a78eefe75b3195db0ba7ea3734c6c5b273ec
cathrina <40246162+cathumali@users.noreply.github.com>
2021-09-02 11:19:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS(withLess(withImages(withSass({
   env: {
-    ANY_ENV_KEY: "ANY_ENV_VARIABLE"
+    ANY_ENV_KEY: "ANY_ENV_VARIABLE",
+    target: 'serverless' 
   }
 }))));
